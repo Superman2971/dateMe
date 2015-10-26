e6a19cc885d4e5ac88a8c39b44e98ed8310551c6
Superman2971 <goldfarb.ian@gmail.com>
2015-10-25 23:25:55 -0700

    still trying to figure out twilio with jquery and not node.js

--- dateMe.js
+++ dateMe.js
@@ -20,21 +20,59 @@ function form() {
   console.log('message me using twillio api');
   document.getElementById('form').style.display = 'none';
   document.getElementById('question1').style.display = 'inline';
+  $.ajax({
+    type: 'POST',
+    username: TWILIO_ACCOUNT_SID,
+    password: TWILIO_AUTH_TOKEN,
+    url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json',
+    data: {
+      'to': '+15416210655',
+      'from': '+15412272133',
+      'body': 'something'
+    },
+    success: function (data) {
+      alert(JSON.stringify(data));
+    },
+    error: function(){
+      alert('Cannot get data');
+    }
+  });
 }
 
-$.ajax({
-  url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/SMS/Messages',
-  type: 'POST',
-  data: {
-    "body": "Jenny please?! I love you <3",
-    "from": "+15412272133",
-    "to": "+15416210655"
-  },
-  beforeSend: function(xhr, settings) {
-    xhr.setRequestHeader('Authorization','Bearer ' + TWILIO_AUTH_TOKEN);
-  },
-  success: function( response ) {
-    // response
-    console.log('something just sent successfully');
-  }
-});
+// $.ajax({
+//   // url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/SMS/Messages',
+//   url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages',
+//   type: 'POST',
+//   data: {
+//     'body': 'Jenny please?! I love you <3',
+//     'from': '+15412272133',
+//     'to': '+15416210655'
+//   },
+//   beforeSend: function(xhr, settings) {
+//     xhr.setRequestHeader('Authorization','Bearer ' + TWILIO_AUTH_TOKEN);
+//   },
+//   success: function( response ) {
+//     // response
+//     console.log('something just sent successfully');
+//   }
+// });
+
+// $.ajax({
+//     type: 'POST',
+//     username: TWILIO_ACCOUNT_SID,
+//     password: TWILIO_AUTH_TOKEN,
+//     url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_ACCOUNT_SID + '/Messages.json',
+//     data: {
+//       'to': '+15416210655',
+//       'from': '+15412272133',
+//       'body': 'something'
+//     },
+//     success: function (data) {
+//       alert(JSON.stringify(data));
+//     },
+//     error: function(){
+//       alert('Cannot get data');
+//     }
+// });
+
+
