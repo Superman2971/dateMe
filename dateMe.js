d657e867b30fc21fe3f3d0c73005e9f7b130c3d2
Superman2971 <goldfarb.ian@gmail.com>
2015-10-25 19:09:56 -0700

    added working javascript for test

--- dateMe.js
+++ dateMe.js
@@ -3,3 +3,18 @@ $(document).ready(function() {
     console.log('working');
   });
 });
+
+// Question #1
+function question1() {
+  console.log('question1');
+}
+
+// Question #2
+function question2() {
+  console.log('question2');
+}
+
+// Question #3
+function question3() {
+  console.log('question3');
+}
